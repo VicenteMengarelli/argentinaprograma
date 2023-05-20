452501b2cc5aaea5b9b3168f0ff4a5920c7736e6
VicenteMengarelli <VicenteMengarellim>
2023-05-20 11:51:33 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,22 @@
+//3. Escribir un programa JavaScript que realice la conversión de grados Fahrenheit
+//(F) a grados Celsius (C) . Deberá guardar este programa en un archivo llamado:
+//conversionGrados.js. Luego, responder a las siguientes preguntas utilizando
+//su código para obtener los resultados. Ayuda: la ecuación de equivalencia está
+//dada por:
+
+//celsius = (fahrenheit − 32) * 5/9
+
+
+//a. ¿Cuántos grados C son 50 F?
+
+fahrenheit=50
+celsius = (fahrenheit-32)*(5/9)
+
+console.log(fahrenheit+" Fahrenheit = "+celsius +" celsius")
+
+//b. ¿Cuántos grados C son 75 F?
+
+fahrenheit=75
+celsius = (fahrenheit-32)*(5/9)
+
+console.log(fahrenheit+" Fahrenheit = "+celsius +" celsius")
