c0e8c7803c52a5859265c2669fa13e19ee18818b
VicenteMengarelli <VicenteMengarellim>
2023-05-20 11:45:48 -0300

    resta.js y multiplicacion.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,17 @@
+//#b. Dado tres números calcule la multiplicación entre ellos y lo imprima por
+//#pantalla. Deberá guardar este programa en un archivo llamado:
+//#multiplicacion.js. Ayuda: utilizar console.log.
+
+let x=3
+let y=5
+let z=8
+
+console.log(x*y*z)
+
+//#● ¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+
+x=2
+y=17
+z=6
+
+console.log(x*y*z)
---
+++ practico0/resta.js
@@ -0,0 +1,19 @@
+//PARTE 1: PEQUEÑOS EJEMPLOS...
+//1. Escribir un programa JavaScript que:
+    //a. Dado dos números calcule la resta entre ambos; donde el primer número
+    //es el minuendo y el segundo es el sustraendo. Imprima el resultado por
+    //pantalla. Deberá guardar este programa en un archivo llamado: resta.js.
+//Ayuda: utilizar console.log.
+
+let x=8
+let y=6
+
+console.log(x-y)
+
+    //● ¿Qué valor devuelve su programa si el minuendo es 10 y el
+    //sustraendo 2?
+
+x=10
+y=2
+
+console.log(x-y)
