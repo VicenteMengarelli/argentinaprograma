471277e18c873a7d0b23d0c21260f4ff718a7081
VicenteMengarelli <VicenteMengarellim>
2023-05-20 11:53:41 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,25 @@
+//2. Escribir un programa JavaScript que calcule el área de un rectángulo. Deberá
+//guardar este programa en un archivo llamado: areaRectangulo.js. Luego,
+//responder a las siguientes preguntas utilizando su código para obtener los
+//resultados. Ayuda: la ecuación del área de un rectángulo está dada por:
+
+//area = base * altura
+
+let base=8
+let altura=6
+
+console.log("Area rectangulo = "+base*altura)
+
+//a. ¿Cuál es el área de un rectángulo de base 5cm y altura 3cm?
+
+base=5
+altura=3
+
+console.log("Area rectangulo = "+base*altura)
+
+//b. ¿Cuál es el área de un rectángulo de base 12.5cm y altura 5.3cm?
+
+base=12.5
+altura=5.3
+
+console.log("Area rectangulo = "+base*altura)
---
+++ practico0/promedio3.js
@@ -0,0 +1,32 @@
+//4. Escribir un programa JavaScript que calcule el promedio de 3 números. Deberá
+//guardar este programa en un archivo llamado: promedio3.js. Luego, responder
+//a las siguientes preguntas utilizando su código para obtener los resultados. La
+//ecuación del promedio entre 3 números está dada por:
+
+//promedio = (x + y + z) / 3
+
+let x=0,y=0,z=0
+
+
+//a. ¿Cuál es el promedio entre 5, 7 y 9?
+x=5
+y=7
+z=9
+
+console.log("Promedio = "+(x+y+z)/3)
+
+//b. ¿Cuál es el promedio entre -2 , -3, -8?
+
+x=-2
+y=-3
+z=-8
+
+console.log("Promedio = "+(x+y+z)/3)
+
+//c. ¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+
+x=-1.2
+y=5.3
+z=-3.3
+
+console.log("Promedio = "+(x+y+z)/3)
