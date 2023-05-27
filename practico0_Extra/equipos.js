f5d5fcb7ac7ffe928ad80cca36c396955c30486b
VicenteMengarelli <VicenteMengarellim>
2023-05-27 12:08:41 -0300

    fin ejercicios extra

---
+++ practico0_Extra/equipos.js
@@ -0,0 +1,163 @@
+//Queremos representar un equipo de fútbol 5. Para ello necesitamos tener el nombre de cada jugador, 
+//su posición (arco, defensa, mediocampo, adelante) y su edad. 
+//¿Cómo representamos a cada jugador? ¿Cómo representamos un equipo? ¿Cómo representamos los 8 equipos que juegan el torneo?
+
+let Messi= {nombre: "Lionel Andres",
+            edad: 34,
+            posicion: "delantero"}
+
+let Martinez= {nombre: "Emiliano",
+            edad: 31,
+            posicion: "arco"}
+
+let Romero= {nombre: "Cristian",
+            edad: 25,
+            posicion: "defensa"}
+
+let DePaul= {nombre: "Rodrigo",
+            edad: 29,
+            posicion: "mediocampo"}
+
+let Alvarez= {nombre: "Julian",
+            edad: 23,
+            posicion: "delantero"}
+
+let equipo1=[Messi, Martinez, Romero, DePaul, Alvarez]
+
+let jugador2={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador3={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador4={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador5 = {nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador6 = {
+            nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+
+let equipo2=[jugador2, jugador3, jugador4, jugador5, jugador6]
+
+let jugador7={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador8={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador9={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador10 = {nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador11 = {
+            nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+
+let equipo3=[jugador7, jugador8, jugador9, jugador10, jugador11]
+
+let jugador12={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador13={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador14={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador15 = {nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador16 = {
+            nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+
+let equipo4=[jugador12, jugador13, jugador14, jugador15, jugador16]
+
+let jugador17={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador18={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador19={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador20 = {nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador21 = {
+            nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+
+let equipo5=[jugador17, jugador18, jugador19, jugador20, jugador21]
+
+let jugador22={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador23={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador24={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador25 = {nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador26 = {
+            nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+
+let equipo6=[jugador22, jugador23, jugador24, jugador25, jugador26]
+
+let jugador27={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador28={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador29={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador30 = {nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador31 = {
+            nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+
+let equipo7=[jugador27, jugador28, jugador29, jugador30, jugador31]
+
+let jugador32={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador33={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador34={nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador35 = {nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+let jugador36 = {
+            nombre: "Jugador",
+            edad: 22,
+            posicion: "delantero"}
+
+let equipo8=[jugador32, jugador33, jugador34, jugador35, jugador36]
+
+
+let Torneo=[equipo1,equipo2,equipo3,equipo4,equipo5,equipo6,equipo7,equipo8]
+
+console.log(Torneo[0][2])
